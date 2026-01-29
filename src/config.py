9a0b00ddb92953fa1f6b7eba88beb0d776dfa3d6
Benjamin Henri Hessler <hesslerbenjamin432@gmail.com>
2026-01-29 16:38:01 +0100

    Hard reset

--- src/config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-# Base directory of project
-BASE_DIR = Path(__file__).resolve().parent
-
-# Paths
-OUTPUT_DIR = BASE_DIR / "output/"
-SAMPLE_DIR = BASE_DIR / "sample"
--- src/main.py
+++ src/main.py
@@ -1,34 +1,5 @@
-import pygame
-
-pygame.init()
-
-# global variables
-width = 600
-height = 600
-display = pygame.display.set_mode((width, height))
-
-
 def main():
-    # game loop
-    loop = True
-    while loop:
-        display.fill((0, 0, 0))
-        # event handler
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                loop = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    hover_toggle = not hover_toggle
-
-                if event.key == pygame.K_q:
-                    loop = False
-                    exit()
-
-        # update the display
-        pygame.display.flip()
-
-
+    pass
+    
 if __name__ == "__main__":
     main()
